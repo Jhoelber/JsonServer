8610b59fc5f4b7ac76334e87a104696473b136ee
Jhoelber <116396136+Jhoelber@users.noreply.github.com>
2024-06-20 11:57:12 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 const filePath = path.join(__dirname, 'db.json'); // Use __dirname para obter o diretório atual corretamente
 const data = fs.readFileSync(filePath, 'utf-8');
@@ -9,21 +10,27 @@ const db = JSON.parse(data);
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 const router = jsonServer.router(db); // Crie o roteador com os dados do arquivo JSON
-server.use(cors());
+
+server.use(cors()); // Habilita o CORS para todas as origens
+
 server.use(middlewares);
+
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }));
-server.use(jsonServer.bodyParser); // Adicione o bodyParser para lidar com JSON no corpo das requisições
 
-// Rota para lidar com operações de escrita (PUT, POST, DELETE)
+// Middleware para permitir operações de escrita apenas na rota '/Usuario'
 server.use('/Usuario', (req, res, next) => {
     if (req.method === 'PUT' || req.method === 'POST' || req.method === 'DELETE') {
-        // Implemente sua lógica de verificação de autorização, autenticação, etc. aqui, se necessário
-        next();
+        // Aqui você pode implementar lógicas de autenticação, autorização, etc.
+        // Por exemplo:
+        // if (!req.headers.authorization) {
+        //     return res.status(401).send('Unauthorized');
+        // }
+        next(); // Passa para o próximo middleware se for uma operação de escrita
     } else {
-        res.sendStatus(404); // Responda com 404 para outros métodos não suportados
+        res.sendStatus(404); // Responde com 404 para outros métodos não suportados
     }
 });
 
