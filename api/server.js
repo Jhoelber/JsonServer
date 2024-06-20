26ae335e0df8e13c565d65bf151e15e30beb02f6
Jhoelber <116396136+Jhoelber@users.noreply.github.com>
2024-06-20 11:54:05 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,31 +1,36 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const fs = require('fs');
+const path = require('path');
 
-const server = jsonServer.create()
+const filePath = path.join(__dirname, 'db.json'); // Use __dirname para obter o diretório atual corretamente
+const data = fs.readFileSync(filePath, 'utf-8');
+const db = JSON.parse(data);
 
-// Uncomment to allow write operations
- const fs = require('fs')
- const path = require('path')
- const filePath = path.join('db.json')
- const data = fs.readFileSync(filePath, "utf-8");
- const db = JSON.parse(data);
- const router = jsonServer.router(db)
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router(db); // Crie o roteador com os dados do arquivo JSON
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
-
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-// Add this before server.use(router)
+server.use(middlewares);
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+}));
+server.use(jsonServer.bodyParser); // Adicione o bodyParser para lidar com JSON no corpo das requisições
 
-// Export the Server API
-module.exports = server
+// Rota para lidar com operações de escrita (PUT, POST, DELETE)
+server.use('/Usuario', (req, res, next) => {
+    if (req.method === 'PUT' || req.method === 'POST' || req.method === 'DELETE') {
+        // Implemente sua lógica de verificação de autorização, autenticação, etc. aqui, se necessário
+        next();
+    } else {
+        res.sendStatus(404); // Responda com 404 para outros métodos não suportados
+    }
+});
+
+server.use(router);
+
+server.listen(3000, () => {
+    console.log('JSON Server is running');
+});
+
+module.exports = server;
